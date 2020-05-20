ad69e8685344a7c88932eba57ab7f6ddc9ec0955
= <=>
2020-05-20 19:13:34 +0200

    add Header exchange type with argv command line

---
+++ 2/queue.py
@@ -0,0 +1,48 @@
+#!/home/ubd/anaconda3/envs/test_env/bin/python
+ 
+import json
+import pika
+import sys
+ 
+if len(sys.argv) < 4:
+	print("Call syntax: python SCRIPT_NAME PRODUCER_NAME MESSAGE_STRING HEADERS_VALUES")
+	exit()
+	
+producerName = sys.argv[1]
+messageText = sys.argv[2]	
+headersValues = sys.argv[3:]
+ 
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+ 
+channel.exchange_declare(exchange='exchange_headers', exchange_type='headers')
+ 
+headers = {}
+number = 1
+for headerValue in headersValues:
+	headers["K" + str(number)] = headerValue
+	number = number + 1
+	
+data = {
+        "producerName": producerName,
+        "headersValues": headersValues,
+        "headers": headers,
+        "messageText": messageText
+    }
+message = json.dumps(data)
+ 
+channel.basic_publish(
+    exchange='exchange_headers',
+    routing_key='',
+    body=message,
+    properties=pika.BasicProperties(
+        delivery_mode=2,
+        headers=headers
+    ))
+    
+print("Producer name: %s, headers: %s, message text:%s" % (producerName, headers, message))
+connection.close()
+
+
+# python queue.py Q1 'any' A C
+# python queue.py Q1 'any' A C
